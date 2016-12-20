b6b2832f0430cc1087d4be67423a03ba87cc4f63
Daniel <danro880@student.liu.se>
2016-12-20 09:56:43 +0100

    Nevermind

--- jsx/Dashboard.jsx
+++ jsx/Dashboard.jsx
@@ -12,7 +12,8 @@ import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 
 const mountNode = document.getElementById('dashboard');
-const baseUrl = "http://localhost:80/api/voting/";
+const baseUrl = "https://beta.d-sektionen.se/api/voting/";
+// const baseUrl = "http://localhost/api/voting/";
 
 class Dashboard extends React.Component {
 
