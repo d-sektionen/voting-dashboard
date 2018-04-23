94c75a208e5b4533edcad14aea89b5efe1431382
JubbeArt <jesperwrang96@gmail.com>
2018-04-24 00:56:08 +0200

    Changed to the way production is detected

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
-const isProd = process.env.NODE_ENV === 'production'
-export const apiURL = isProd ? 'https://api.d-sektionen.se' : 'https://dsek-api-dev.herokuapp.com'
-export const socketURL = isProd ? 'wss://api.d-sektionen.se' : 'wss://dsek-api-dev.herokuapp.com'
+
+const isDev = process.env.NODE_ENV === 'development'
+
+export const apiURL = isDev ? 'https://dsek-api-dev.herokuapp.com' : 'https://api.d-sektionen.se'
+export const socketURL = isDev ? 'wss://dsek-api-dev.herokuapp.com' : 'wss://api.d-sektionen.se'
 export const loginURL = `${apiURL}/account/token?redirect=${window.location.href}`
